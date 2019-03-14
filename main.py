1b90088827587f7db27832edd2ebec5019c5323c
Kasey Martin <kasey.hackspace@gmail.com>
2019-03-14 10:19:45 +0800

    reads arduino serial prints

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+Python to arduino serial sample
+"""
+import serial
+import time
+__author__ = "Hackspace"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    ser = serial.Serial('/dev/cu.usbserial-14110', 9600, timeout=1)
+    ser.reset_output_buffer()
+    time.sleep(1)
+    while True:
+        ser.write(b'1')
+        time.sleep(1)
+        line = ser.readline()
+        print(line.decode('unicode_escape'))
+
+        '''
+        ser.write(b'0')
+        time.sleep(1)
+        line = ser.readline()
+        print(line.decode('unicode_escape'))
+        '''
+    ser.close()
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
